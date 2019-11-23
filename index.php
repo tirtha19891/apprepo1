45135971ee6a7e77fc19da86bf154a78ffa9fe4f
tirtha19891 <48406136+tirtha19891@users.noreply.github.com>
2019-11-23 13:57:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo "Welcome Tirtho1 to My Azure App Server Hosted Site";
 
 echo "<br>";
-<img src="https://tirthastorage.blob.core.windows.net/tirthacontainer/Screenshot%20from%202019-09-01%2015-58-59.png" alt="Smiley face" height="552" width="552">
+#<img src="https://tirthastorage.blob.core.windows.net/tirthacontainer/Screenshot%20from%202019-09-01%2015-58-59.png" alt="Smiley face" height="552" width="552">
 #phpinfo();
 ?>
